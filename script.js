daa58a673e5e3a422bb8ae021d0c21b9e5ee4a97
Aigul <sharipova.aig@gmail.com>
2022-08-01 21:19:28 -0400

    function

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const button = document.querySelector('#btn1');
 const buttonTwo = document.querySelector('#btn2');
+
+button.addEventListener('click', calculate);
+function calculate() {
+  const bill = document.querySelector('#bill').value;
+  const people= document.querySelector('#bill').value;
+  const tip = document.querySelector('#tip').value;
+}
